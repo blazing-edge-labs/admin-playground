7ff4de63813cbe4e34a1616442096ca968932312
Mateo Karadza <mateo@eastcoastproduct.com>
2017-10-12 14:29:26 +0200

    combining inline styles into a single css file in production mode Fixes T14

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,13 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   devtool: 'cheap-module-source-map',
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
+    filename: '[name]-[chunkhash].js',
     publicPath: '/'
   },
   resolve: {
@@ -26,6 +27,7 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify('production'),
       __API_URL__: JSON.stringify(process.env.API_URL)
     }),
+    new ExtractTextPlugin('style.css', { allChunks: true }),
     new webpack.optimize.CommonsChunkPlugin('common-[chunkhash].js'),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin()
@@ -41,9 +43,9 @@ module.exports = {
         use: 'json'
       }, {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          {
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
             loader: 'css-loader',
             options: {
               modules: true, // default is false
@@ -51,12 +53,14 @@ module.exports = {
               importLoaders: 1,
               localIdentName: '[name]--[local]--[hash:base64:8]'
             }
-          },
-          'postcss-loader'
-        ]
+          }, 'postcss-loader']
+        })
       }, {
         test: /\.sass$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
       }, {
         test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
         use: 'url-loader?limit=10000&mimetype=application/font-woff'
